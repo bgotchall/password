f87fa5e94c3607fca79e7a2dba4a15e7bf380b72
austinev <bgotchall@gmail.com>
2019-10-26 12:01:59 -0700

    really adding the code this time

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //alert("hi");
+
+
+var generate_buttonEl=document.querySelector("#generate_button");
+var output_boxEl=document.querySelector("#output_box");
+
+
+var lengthEl=document.querySelector("#password_length");
+var SpecialEl=document.querySelector("#special_char_Check");
+// var SpecialEl=document.querySelector("#special_char_Check");
+// var SpecialEl=document.querySelector("#special_char_Check");
+
+
+
+generate_buttonEl.onclick= function (element){
+
+    alert("clicky");
+
+
+};
+
+SpecialEl.onclick=function(){
+
+    alert("clicksters");
+};
